1d349407016b7275db16faa96794d3f4c4e8d0b5
Huai <huai@HuaideMBP.lan>
2020-10-23 22:01:39 +0800

    00000B817106175378F600000201321245610006B49AED86CB01

--- sms.py
+++ sms.py
@@ -5,7 +5,7 @@ import copy
 from smspdu.codecs import GSM, UCS2
 
 def decode_pdu(data: bytearray):
-    result = UCS2.decode(data)
+    result = GSM.decode(data)
     logging.info('result:', result)
     return result
     # result = dict()
