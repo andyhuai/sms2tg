a699b66bc8d51cb6afa3c753022ec2b0a2313098
Huai <huai@HuaideMBP.lan>
2020-10-25 15:44:00 +0800

    -c

--- send.py
+++ send.py
@@ -5,7 +5,7 @@ import os
 
 
 def send():
-    os.system('gammu-smsd-inject -c /etc/gammu-smsdrc-cdma TEXT 17607135876 -unicode -text "test----中文"')
+    os.system('gammu-smsd-inject -c /etc/gammu-smsdrc TEXT 17607135876 -unicode -text "test----中文"')
 
 
 if __name__ == '__main__':
