79d1358971e02c9126ad581cf1d34c7e04e5ef43
Huai <huai@HuaideMBP.lan>
2020-10-25 15:59:16 +0800

    sendCdma

--- send.py
+++ send.py
@@ -5,11 +5,11 @@ import os
 
 
 def send():
-    os.system('gammu-smsd-inject -c /etc/gammu-smsdrc TEXT 17607135876 -unicode -text "test----中文"')
+    os.system('gammu-smsd-inject -c /etc/gammu-smsdrc TEXT 17607135876 -unicode -text "test----中文-联通"')
 
 
 def sendCdma():
-    os.system('gammu-smsd-inject -c /etc/gammu-smsdrc-cdma TEXT 17607135876 -unicode -text "test----中文"')
+    os.system('gammu-smsd-inject -c /etc/gammu-smsdrc-cdma TEXT 17607135876 -unicode -text "test----中文-电信"')
 
 
 if __name__ == '__main__':
