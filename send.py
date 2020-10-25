2fc530c5e9cc9f11c6e8fd887c1b874e698fabf5
Huai <huai@HuaideMBP.lan>
2020-10-25 15:36:51 +0800

    send

--- send.py
+++ send.py
@@ -1,24 +1,12 @@
 #!/usr/bin/env python
 # encoding: utf-8
 import time
-import gammu.smsd
-import sys
-
-receptNum = '+8617607135876' #自己的电话号码
-smsd = gammu.smsd.SMSD('/etc/gammu-smsdrc')
+import os
 
 
-def send_message(msg):
-  message = {
-     'Text': msg,
-     'SMSC': {'Location': 1},
-     'Number': receptNum,
-     'Coding': 'Unicode_No_Compression'
-   }
-  #print(message)
-  smsd.InjectSMS([message])
+def send():
+    os.system("sudo gammu-smsd-inject -c /var/gammu-smsdrc-cdma TEXT 17607135876 -text '测试一下'")
 
 
 if __name__ == '__main__':
-  # send_message('测试一下')
-  smsd.Shutdown()
+    send()
